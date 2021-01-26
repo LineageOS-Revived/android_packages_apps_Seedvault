8480f0fdd6f9a5159e64123f9e4e7be03cac8921
Chirayu Desai <chirayudesai1@gmail.com>
2021-01-26 13:31:58 +0530

    Only treat eng builds as debug
    * This automatically fills in the 12-word keyphrase prompt, and
      was meant only for quick testing during development.
    * Since LineageOS release builds are userdebug, just check for eng instead.
    
    Change-Id: Ie19a4177c180e7d34e2d14fb8b588ffe61af4221

--- app/src/main/java/com/stevesoltys/seedvault/App.kt
+++ app/src/main/java/com/stevesoltys/seedvault/App.kt
@@ -88,4 +88,4 @@ const val MAGIC_PACKAGE_MANAGER = PACKAGE_MANAGER_SENTINEL
 const val ANCESTRAL_RECORD_KEY = "@ancestral_record@"
 const val GLOBAL_METADATA_KEY = "@meta@"
 
-fun isDebugBuild() = Build.TYPE == "userdebug"
+fun isDebugBuild() = Build.TYPE == "eng"
